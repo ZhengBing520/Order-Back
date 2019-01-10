66ca49afefc12e64745b255e51f03fb1619a241d
Zhengbign <505986526@qq.com>
2019-01-10 12:29:21 +0800

    1

--- src/main/java/com/zb/dto/BusinessDto.java
+++ src/main/java/com/zb/dto/BusinessDto.java
@@ -16,6 +16,11 @@ public class BusinessDto extends BaseDto{
      */
     private Integer cardId;
 
+    /**
+     * 卡号信息
+     */
+    private String cardNum;
+
     public String getName() {
         return name;
     }
@@ -30,5 +35,13 @@ public class BusinessDto extends BaseDto{
 
     public void setCardId(Integer cardId) {
         this.cardId = cardId;
+    }
+
+    public String getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(String cardNum) {
+        this.cardNum = cardNum;
     }
 }
